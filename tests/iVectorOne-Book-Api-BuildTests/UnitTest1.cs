05d7960916cbb21b9962d33f6eb6508700581848
Craig Shepperson <craig@intuitivesystems.co.uk>
2022-05-25 14:33:22 +0100

    Add reamining build test projects

---
+++ tests/iVectorOne-Book-Api-BuildTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace iVectorOne_Book_Api_BuildTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/iVectorOne-Search-Api-BuildTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace iVectorOne_Search_Api_BuildTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
