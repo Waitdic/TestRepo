420707a6ca72746f894474cf08faf3cf5d370ca9
craig <craig@intuitivesystems.co.uk>
2022-06-23 13:27:46 +0000

    Change token identifier from [ to {

--- build/scripts/DeployUpdate.bat
+++ build/scripts/DeployUpdate.bat
@@ -1,7 +1,7 @@
 (
-    robocopy [source] [destination]] /s
-    rmdir [source] /s /q
+    robocopy {source} {destination} /s
+    rmdir {source} /s /q
 
-    C:\Windows\System32\inetsrv\appcmd.exe recycle apppool [apppool]
+    C:\Windows\System32\inetsrv\appcmd.exe recycle apppool {apppool}
 
 ) ^& IF %ERRORLEVEL% LEQ 1 exit 0
