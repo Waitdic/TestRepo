c2be7fcfa128855ee515af5400290609bddd0c1d
Zoltan Busi <zoltan.busi@ap4.hu>
2022-07-05 15:43:56 +0200

    185 - Update Support pages

--- src/iVectorOne-Admin-Web/src/libs/core/support/knowledge-base/index.tsx
+++ src/iVectorOne-Admin-Web/src/libs/core/support/knowledge-base/index.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 const KnowledgeBase: React.FC<Props> = ({}) => {
   return (
-    <Main padding='pb-8' authGuard>
+    <Main padding='pb-8'>
       {/* Search area */}
       <div className='relative flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 py-8 lg:py-16 bg-indigo-500 overflow-hidden'>
         {/* Glow */}
@@ -104,9 +104,7 @@ const KnowledgeBase: React.FC<Props> = ({}) => {
           {/* Popular Topics */}
           <div>
             <div className='mb-5'>
-              <h2 className='text-xl text-slate-800 font-bold'>
-                Popular Topics
-              </h2>
+              <h2 className='text-xl text-slate-800 font-bold'>Quick Links</h2>
             </div>
             {/* Grid */}
             <div className='grid sm:grid-cols-2 lg:grid-cols-4 lg:sidebar-expanded:grid-cols-2 xl:sidebar-expanded:grid-cols-4 gap-6'>
@@ -149,19 +147,192 @@ const KnowledgeBase: React.FC<Props> = ({}) => {
                     </div>
                     {/* Content */}
                     <h3 className='text-lg font-semibold text-slate-800 mb-1'>
-                      Getting Started
+                      Integration Support
                     </h3>
                     <div className='text-sm'>
-                      Lorem ipsum dolor sit consectetur adipiscing elit sed do.
+                      For help and support during your integration, please email
+                      the team and we’ll do our best to assist.
                     </div>
                   </div>
                   {/* Link */}
                   <div>
                     <a
                       className='text-sm font-medium text-indigo-500 hover:text-indigo-600'
-                      href='#0'
+                      href='mailto:helpdesk@ivectorone.com'
                     >
-                      Explore -&gt;
+                      helpdesk@ivectorone.com
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className='bg-slate-100 rounded-sm text-center p-5'>
+                <div className='flex flex-col h-full'>
+                  <div className='grow mb-2'>
+                    {/* Icon */}
+                    <div className='inline-flex w-12 h-12 rounded-full bg-indigo-400'>
+                      <svg
+                        className='w-12 h-12'
+                        viewBox='0 0 48 48'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <defs>
+                          <linearGradient
+                            x1='50%'
+                            y1='0%'
+                            x2='50%'
+                            y2='100%'
+                            id='icon1-a'
+                          >
+                            <stop stopColor='#FFF' offset='0%' />
+                            <stop stopColor='#A5B4FC' offset='100%' />
+                          </linearGradient>
+                        </defs>
+                        <g fillRule='nonzero' fill='none'>
+                          <path
+                            d='M19.236 21.995h-3.333c-.46 0-.833.352-.833.786v9.428c0 .434.373.786.833.786h4.167V22.78c0-.434-.374-.786-.834-.786Z'
+                            fill='#4F46E5'
+                            opacity='.88'
+                          />
+                          <path
+                            d='M34.234 20.073a2.393 2.393 0 0 0-.735-.116h-5v-2.609c0-3.325-2.157-4.297-3.298-4.347a.828.828 0 0 0-.611.24.888.888 0 0 0-.257.63v4.032L21 22.077v10.924h10.19c1.1.005 2.073-.744 2.392-1.842l2.308-7.826a2.711 2.711 0 0 0-.181-1.988 2.528 2.528 0 0 0-1.475-1.272Z'
+                            fill='url(#icon1-a)'
+                            transform='translate(-.93 -.005)'
+                          />
+                        </g>
+                      </svg>
+                    </div>
+                    {/* Content */}
+                    <h3 className='text-lg font-semibold text-slate-800 mb-1'>
+                      Swagger Documentation
+                    </h3>
+                    <div className='text-sm'>
+                      To create a client SDK, please download the YAML file,
+                      which can then be loaded into the Swagger Editor and then
+                      transformed into your preferred language
+                    </div>
+                  </div>
+                  {/* Link */}
+                  <div>
+                    <a
+                      className='text-sm font-medium text-indigo-500 hover:text-indigo-600'
+                      target='_blank'
+                      href='https://app.swaggerhub.com/apis-docs/Intuitivelimited/iVectorOne/v1'
+                    >
+                      View
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className='bg-slate-100 rounded-sm text-center p-5'>
+                <div className='flex flex-col h-full'>
+                  <div className='grow mb-2'>
+                    {/* Icon */}
+                    <div className='inline-flex w-12 h-12 rounded-full bg-indigo-400'>
+                      <svg
+                        className='w-12 h-12'
+                        viewBox='0 0 48 48'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <defs>
+                          <linearGradient
+                            x1='50%'
+                            y1='0%'
+                            x2='50%'
+                            y2='100%'
+                            id='icon1-a'
+                          >
+                            <stop stopColor='#FFF' offset='0%' />
+                            <stop stopColor='#A5B4FC' offset='100%' />
+                          </linearGradient>
+                        </defs>
+                        <g fillRule='nonzero' fill='none'>
+                          <path
+                            d='M19.236 21.995h-3.333c-.46 0-.833.352-.833.786v9.428c0 .434.373.786.833.786h4.167V22.78c0-.434-.374-.786-.834-.786Z'
+                            fill='#4F46E5'
+                            opacity='.88'
+                          />
+                          <path
+                            d='M34.234 20.073a2.393 2.393 0 0 0-.735-.116h-5v-2.609c0-3.325-2.157-4.297-3.298-4.347a.828.828 0 0 0-.611.24.888.888 0 0 0-.257.63v4.032L21 22.077v10.924h10.19c1.1.005 2.073-.744 2.392-1.842l2.308-7.826a2.711 2.711 0 0 0-.181-1.988 2.528 2.528 0 0 0-1.475-1.272Z'
+                            fill='url(#icon1-a)'
+                            transform='translate(-.93 -.005)'
+                          />
+                        </g>
+                      </svg>
+                    </div>
+                    {/* Content */}
+                    <h3 className='text-lg font-semibold text-slate-800 mb-1'>
+                      Postman Collection
+                    </h3>
+                    <div className='text-sm'>
+                      This contains examples of each call referenced in the API
+                      documentation, but with examples relevant to your account
+                    </div>
+                  </div>
+                  {/* Link */}
+                  <div>
+                    <a
+                      className='text-sm font-medium text-indigo-500 hover:text-indigo-600'
+                      target='_blank'
+                      href='https://documenter.getpostman.com/view/2159662/UyxqD49x'
+                    >
+                      View
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className='bg-slate-100 rounded-sm text-center p-5'>
+                <div className='flex flex-col h-full'>
+                  <div className='grow mb-2'>
+                    {/* Icon */}
+                    <div className='inline-flex w-12 h-12 rounded-full bg-indigo-400'>
+                      <svg
+                        className='w-12 h-12'
+                        viewBox='0 0 48 48'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <defs>
+                          <linearGradient
+                            x1='50%'
+                            y1='0%'
+                            x2='50%'
+                            y2='100%'
+                            id='icon1-a'
+                          >
+                            <stop stopColor='#FFF' offset='0%' />
+                            <stop stopColor='#A5B4FC' offset='100%' />
+                          </linearGradient>
+                        </defs>
+                        <g fillRule='nonzero' fill='none'>
+                          <path
+                            d='M19.236 21.995h-3.333c-.46 0-.833.352-.833.786v9.428c0 .434.373.786.833.786h4.167V22.78c0-.434-.374-.786-.834-.786Z'
+                            fill='#4F46E5'
+                            opacity='.88'
+                          />
+                          <path
+                            d='M34.234 20.073a2.393 2.393 0 0 0-.735-.116h-5v-2.609c0-3.325-2.157-4.297-3.298-4.347a.828.828 0 0 0-.611.24.888.888 0 0 0-.257.63v4.032L21 22.077v10.924h10.19c1.1.005 2.073-.744 2.392-1.842l2.308-7.826a2.711 2.711 0 0 0-.181-1.988 2.528 2.528 0 0 0-1.475-1.272Z'
+                            fill='url(#icon1-a)'
+                            transform='translate(-.93 -.005)'
+                          />
+                        </g>
+                      </svg>
+                    </div>
+                    {/* Content */}
+                    <h3 className='text-lg font-semibold text-slate-800 mb-1'>
+                      API Documentation
+                    </h3>
+                    <div className='text-sm'>
+                      All of the iVectorOne API requests and responses explained
+                      in detail
+                    </div>
+                  </div>
+                  {/* Link */}
+                  <div>
+                    <a
+                      className='text-sm font-medium text-indigo-500 hover:text-indigo-600'
+                      target='_blank'
+                      href='https://ivectorone.com/wp-content/uploads/2021/09/iVectorOne-API-Documentation.pdf'
+                    >
+                      View
                     </a>
                   </div>
                 </div>
@@ -172,9 +343,7 @@ const KnowledgeBase: React.FC<Props> = ({}) => {
           {/* Popular Guides */}
           <div>
             <div className='mb-5'>
-              <h2 className='text-xl text-slate-800 font-bold'>
-                Popular Guides
-              </h2>
+              <h2 className='text-xl text-slate-800 font-bold'>Guides</h2>
             </div>
             {/* Grid */}
             <div className='grid sm:grid-cols-2 gap-6'>
@@ -192,20 +361,21 @@ const KnowledgeBase: React.FC<Props> = ({}) => {
                     {/* Content */}
                     <div className='grow mb-2'>
                       <div className='font-semibold text-slate-800 mb-1'>
-                        Getting Started
+                        API Documentation
                       </div>
                       <div className='text-sm'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing sed
-                        do eiusmod tempor incididunt ut labore et dolore.
+                        All of the iVectorOne API requests and responses
+                        explained in detail
                       </div>
                     </div>
                     {/* Link */}
                     <div>
                       <a
                         className='text-sm font-medium text-indigo-500 hover:text-indigo-600'
-                        href='#0'
+                        target='_blank'
+                        href='https://ivectorone.com/wp-content/uploads/2021/09/iVectorOne-API-Documentation.pdf'
                       >
-                        View -&gt;
+                        View
                       </a>
                     </div>
                   </div>
