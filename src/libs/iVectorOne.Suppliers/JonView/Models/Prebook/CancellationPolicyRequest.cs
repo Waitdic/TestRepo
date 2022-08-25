090475f45fe796d0ac890427167a33a0c13f0404
Danis Yakupov <waitdic161616@gmail.com>
2022-08-25 15:51:08 +0300

    705 - fix bags

---
+++ src/libs/iVectorOne.Suppliers/JonView/Models/Prebook/CancellationPolicyRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace iVectorOne.Suppliers.JonView.Models.Prebook
+{
+    using System.Xml.Serialization;
+
+    [XmlRoot("message")]
+    public class CancellationPolicyRequest : IMessageRq
+    {
+        [XmlElement("actionseg")]
+        public string ActionSeg { get; set; } = string.Empty;
+
+        [XmlElement("searchseg")]
+        public PrebookSearchSegment SearchSeg { get; set; } = new();
+    }
+}
