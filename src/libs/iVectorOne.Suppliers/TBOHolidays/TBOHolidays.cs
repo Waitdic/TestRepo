a9e145718809f2d775b2a173d057fa0ef1e37ce6
Andrey Gordeev <Andrey.Gordeev@waveaccess.global>
2022-10-10 20:27:47 +0300

    460 - fix book

--- src/libs/iVectorOne.Suppliers/TBOHolidays/TBOHolidays.cs
+++ src/libs/iVectorOne.Suppliers/TBOHolidays/TBOHolidays.cs
@@ -359,6 +359,7 @@
 
                 var bookingDetailRequest = new Envelope<HotelBookingDetailRequest>();
                 bookingDetailRequest.Body.Content.BookingId = propertyDetails.SourceSecondaryReference.Split('-')[0].ToSafeInt();
+                bookingDetailRequest.Body.Content.ConfirmationNo = propertyDetails.SourceReference;
                 bookingDetailRequest.Header = BuildHeader("HotelBookingDetail", propertyDetails, _settings);
 
                 var bookingDetailWebRequest = CreateRequest("HotelBookingDetail", _settings.GenericURL(propertyDetails));
@@ -371,6 +372,13 @@
                 var policies = bookingDetailResponse.BookingDetail.HotelCancelPolicies;
                 if (bookingDetailResponse.Status.StatusCode == "01" && policies.PolicyFormat == PolicyFormat.Nodes)
                 {
+                    if (!string.IsNullOrEmpty(bookingDetailResponse.BookingDetail.BookingId) 
+                        && propertyDetails.SourceSecondaryReference.Split('-')[0].ToSafeInt() == 0)
+                    {
+                        propertyDetails.SourceSecondaryReference =
+                            $"{bookingDetailResponse.BookingDetail.BookingId}-{propertyDetails.SourceSecondaryReference.Split('-')[1]}";
+                    }
+
                     foreach (var cancelPolicy in policies.CancelPolicies.OfType<CancelPolicy>().OrderBy(n => n.FromDate.ToSafeDate()))
                     {
                         if (DateTime.Now >= cancelPolicy.FromDate.ToSafeDate()
@@ -633,7 +641,8 @@
                     Email = propertyDetails.LeadGuestEmail,
                     City = propertyDetails.LeadGuestTownCity,
                     State = propertyDetails.LeadGuestCounty,
-                    Country = await _support.TPCountryCodeLookupAsync(propertyDetails.Source, propertyDetails.LeadGuestCountryCode, propertyDetails.AccountID),
+                    //Country = await _support.TPCountryCodeLookupAsync(propertyDetails.Source, propertyDetails.LeadGuestCountryCode, propertyDetails.AccountID),
+                    Country = guestNationality,
                     ZipCode = propertyDetails.LeadGuestPostcode,
                 },
                 PaymentInfo = new PaymentInfo
