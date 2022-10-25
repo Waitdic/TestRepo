8737fff5a6e41bd94d33e2e7f25de556a4e3fa4f
craig <craig@intuitivesystems.co.uk>
2022-10-25 13:50:30 +0000

    Merged PR 115: Move endpoints from program cs

--- src/iVectorOne-Admin-Api/Features/V2/Suppliers/Info/Endpoints.cs
+++ src/iVectorOne-Admin-Api/Features/V2/Suppliers/Info/Endpoints.cs
@@ -2,13 +2,13 @@
 using iVectorOne_Admin_Api.Config.Responses;
 using iVectorOne_Admin_Api.Security;
 
-namespace iVectorOne_Admin_Api.Features.V1.Suppliers.Info
+namespace iVectorOne_Admin_Api.Features.V2.Suppliers.Info
 {
     public static class Endpoints
     {
-        public static IEndpointRouteBuilder MapSupplierInfoV1Endpoint(this IEndpointRouteBuilder endpoints)
+        public static IEndpointRouteBuilder MapSupplierInfoV2Endpoint(this IEndpointRouteBuilder endpoints)
         {
-            _ = endpoints.MapGet("v1/suppliers/{supplierid}", async (IMediator mediator, HttpContext httpContext, int supplierid) =>
+            _ = endpoints.MapGet("v2/suppliers/{supplierid}", async (IMediator mediator, HttpContext httpContext, int supplierid) =>
             {
                 if (httpContext.User.Identity is not TenantIdentity identity)
                 {
