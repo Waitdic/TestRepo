cf7afc7967f4bc76a0c03308d2b806a3b61e993c
Craig Shepperson <craig@intuitivesystems.co.uk>
2022-10-27 15:57:48 +0100

    Fire and forget supplier test

--- src/iVectorOne-Admin-Api/Features/V1/Tenants/Accounts/Suppliers/Test/Endpoints.cs
+++ src/iVectorOne-Admin-Api/Features/V1/Tenants/Accounts/Suppliers/Test/Endpoints.cs
@@ -1,4 +1,6 @@
-﻿namespace iVectorOne_Admin_Api.Features.V1.Tenants.Accounts.Suppliers.Test
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace iVectorOne_Admin_Api.Features.V1.Tenants.Accounts.Suppliers.Test
 {
     public static class Endpoints
     {
@@ -11,7 +13,7 @@
                     int accountId,
                     int supplierId) =>
                 {
-                    var response = await mediator.Send(new Request
+                    var response = await mediator.Send(new Post.Request
                     {
                         AccountID = accountId,
                         SupplierID = supplierId,
@@ -19,6 +21,22 @@
 
                     return response.Result;
 
+                }).RequireAuthorization();
+
+            _ = endpoints.MapGet("v1/tenants/{tenantid}/accounts/{accountid}/suppliers/{supplierid}/test", async (
+                IMediator mediator,
+                int accountId, int supplierId,
+                [FromQuery] string q) =>
+                {
+                    var request = new Get.Request
+                    {
+                        AccountID = accountId,
+                        RequestKey = q
+                    };
+
+                    var response = await mediator.Send(request);
+
+                    return response.Result;
                 }).RequireAuthorization();
 
             return endpoints;
---
+++ src/iVectorOne-Admin-Api/Features/V1/Tenants/Accounts/Suppliers/Test/Get/Handler.cs
@@ -0,0 +1,88 @@
+﻿using FluentValidation;
+using iVectorOne_Admin_Api.Features.Shared;
+
+namespace iVectorOne_Admin_Api.Features.V1.Tenants.Accounts.Suppliers.Test.Get
+{
+    public class Handler : IRequestHandler<Request, ResponseBase>
+    {
+        private readonly AdminContext _context;
+        public Handler(AdminContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResponseBase> Handle(Request request, CancellationToken cancellationToken)
+        {
+            var response = new ResponseBase();
+
+            var account = _context.Accounts.Where(a => a.AccountId == request.AccountID)
+                .Include(a => a.AccountSuppliers)
+                .ThenInclude(a => a.Supplier)
+                .AsNoTracking()
+                .FirstOrDefault();
+
+            if (account == null)
+            {
+                response.NotFound("Account not found.");
+                return response;
+            }
+
+            var results = await _context.FireForgetSearchResponses
+                .Where(x => x.FireForgetSearchResponseKey == request.RequestKey)
+                .ToListAsync(cancellationToken: cancellationToken);
+
+            if (results.Count == 0)
+            {
+                response.NotReady();
+                return response;
+            }
+
+            if (results.Any(x => x.SearchStatus.ToLower() == "ok"))
+            {
+                response.Ok(new ResponseModel { Success = true, Message = "Success. The supplier is returning results." });
+                return response;
+            }
+            else
+            {
+                response.Ok(new ResponseModel { Success = true, Message = $"Failed with error: {string.Join(",", results.Any(x => x.Information.Length != 0))}" });
+                return response;
+            }
+
+            //    var cancelSearch = false;
+            //    var searchRequest = new Adaptors.Search.Request
+            //    {
+            //        Searchdate = searchDate.AddMonths(i * 3),
+            //        Properties = supplier.TestPropertyIDs,
+            //        Login = account.Login,
+            //        Password = account.EncryptedPassword,
+            //        DedupeMethod = "none",
+            //        RoomRequest = "(2,0,0)"
+            //    };
+
+            //    var result = await _searchAdaptor.Execute(searchRequest, cancellationToken);
+            //    //var result = await ExecuteSearch(supplier.TestPropertyIDs, searchDate.AddMonths(i * 3), account.Login, account.EncryptedPassword);
+            //    switch (result.SearchStatus)
+            //    {
+            //        case Adaptors.Search.Response.SearchStatusEnum.Ok:
+            //            message = "Success. The supplier is returning results.";
+            //            break;
+            //        case Adaptors.Search.Response.SearchStatusEnum.Exception:
+            //            message = $"Failed with unexpected error: {result.Information}";
+            //            cancelSearch = true;
+            //            break;
+            //        case Adaptors.Search.Response.SearchStatusEnum.NotOk:
+            //            message = $"Failed with error: {result.Information}";
+            //            cancelSearch = true;
+            //            break;
+            //    };
+
+            //    if (cancelSearch)
+            //    {
+            //        break;
+            //    }
+
+            response.Ok(new ResponseModel { Success = true, Message = "" });
+            return response;
+        }
+    }
+}
