67595dc8a5a3ae50f35d6aae9dd0f1656d6656ea
Craig Shepperson <craig@intuitivesystems.co.uk>
2022-10-28 12:24:31 +0100

    Add booking summary to dashboard

---
+++ src/iVectorOne-Admin-Api/Data/Models/Dashboard/DashboardSummary.cs
@@ -0,0 +1,24 @@
+﻿namespace iVectorOne_Admin_Api.Data.Models.Dashboard
+{
+    public class DashboardSummary
+    {
+        public string Row { get; set; } = string.Empty; 
+
+        public int BookingTotal { get; set; }
+
+        public decimal BookingValue { get; set; }
+
+        public int PrebookTotal { get; set; }
+
+        public int PrebookSuccess { get; set; }
+
+        public int SearchTotal { get; set; }
+
+        public int SearchSuccessful { get; set; }
+
+        public int AverageSearchTime { get; set; }
+
+
+
+    }
+}
