fc18756e6624faafabff432cabca6556268ead4a
nayan <nayan@intuitivesystems.co.uk>
2022-11-08 14:31:27 +0000

    Merged PR 130: Returning Response and Request Log SupplierAPILog

---
+++ src/iVectorOne-Admin-Api/Data/Models/DashboardGraph.cs
@@ -0,0 +1,9 @@
+﻿namespace iVectorOne_Admin_Api.Data.Models
+{
+    public partial class DashboardGraph
+    {
+        public string YValue { get; set; } = string.Empty;
+        public string Series { get; set; } = string.Empty;
+        public int XValue { get; set; }
+    }
+}
---
+++ src/iVectorOne-Admin-Api/Data/Models/DashboardTable.cs
@@ -0,0 +1,22 @@
+﻿namespace iVectorOne_Admin_Api.Data.Models
+{
+    public partial class DashboardTable
+    {
+        public int SearchStoreID { get; set; }
+        public string AccountName { get; set; } = string.Empty;
+        public int AccountID { get; set; }
+        public string System { get; set; } = string.Empty;
+        public bool Successful { get; set; }
+        public string SearchDateAndTime { get; set; } = string.Empty;
+        public int PropertiesRequested { get; set; }
+        public int PropertiesReturned { get; set; }
+        public int PreprocessTime { get; set; }
+        public int MaxSupplierTime { get; set; }
+        public int MaxDedupeTime { get; set; }
+        public int PostProcessTime { get; set; }
+        public int TotalTime { get; set; }
+
+        public Tenant? Tenant { get; set; }
+        public Account? Account { get; set; }
+    }
+}
