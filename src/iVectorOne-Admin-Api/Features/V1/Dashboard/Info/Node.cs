3fa1127d12096f15c5c11743c24b66184af3e2b2
Craig Shepperson <craig@intuitivesystems.co.uk>
2022-11-16 08:04:13 +0000

    Return null for future search dates

--- src/iVectorOne-Admin-Api/Features/V1/Dashboard/Info/Node.cs
+++ src/iVectorOne-Admin-Api/Features/V1/Dashboard/Info/Node.cs
@@ -9,15 +9,5 @@ namespace iVectorOne_Admin_Api.Features.V1.Dashboard.Info
         public int? CurrentTotal { get; set; }
 
         public int PreviousTotal { get; set; }
-        //public string Name { get; set; }
-        //public int SearchTotal { get; set; }
-        //public int SearchSuccess { get; set; }
-        //public int AvgResponse { get; set; }
-        //public int PrebookTotal { get; set; }
-        //public int PrebookSuccess { get; set; }
-        //public int BookTotal { get; set; }
-        //public int BookSuccess { get; set; }
-        //public int S2B { get; set; }
-
     }
 }
