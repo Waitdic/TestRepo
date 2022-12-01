7a039031d13e9614b86db95b04effc3c53e7b025
Ashish Sharma <ashish.sharma18@nagarro.com>
2022-12-01 16:00:29 +0530

    IDT-1293-Book-BuildingTheRequest | interim Unit Test changes checked in

--- src/libs/iVectorOne.Suppliers/TourPlanTransfers/TourPlanTransfersBase.cs
+++ src/libs/iVectorOne.Suppliers/TourPlanTransfers/TourPlanTransfersBase.cs
@@ -11,6 +11,20 @@
     using iVectorOne.SDK.V2;
     using System;
     using System.Collections.Generic;
+    using Intuitive.Helpers.Net;
+    using iVectorOne.Suppliers.Xml.W2M;
+    using Newtonsoft.Json;
+    using iVectorOne.Suppliers.TourPlanTransfers.Models;
+    using System.Linq;
+    using System.Xml;
+    using System.Xml.Serialization;
+    using Intuitive.Helpers.Serialization;
+    using NodaTime.TimeZones;
+    using System.Globalization;
+    using iVectorOne.Suppliers.GoGlobal.Models;
+    using System.Xml.Linq;
+    using iVectorOne.Suppliers.Netstorming.Models.Common;
+    using iVectorOne.Models.Property.Booking;
 
     public abstract class TourPlanTransfersBase : IThirdParty, ISingleSource
     {
@@ -19,16 +33,19 @@
         private ITourPlanTransfersSettings _settings;
         private readonly HttpClient _httpClient;
         private readonly ILogger<TourPlanTransfersSearchBase> _logger;
+        private readonly ISerializer _serializer;
         public const string InvalidSupplierReference = "Invalid Supplier Reference";
 
         public TourPlanTransfersBase(
             ITourPlanTransfersSettings settings,
             HttpClient httpClient,
-            ILogger<TourPlanTransfersSearchBase> logger)
+            ILogger<TourPlanTransfersSearchBase> logger,
+            ISerializer serializer)
         {
             _settings = Ensure.IsNotNull(settings, nameof(settings));
             _httpClient = Ensure.IsNotNull(httpClient, nameof(httpClient));
             _logger = Ensure.IsNotNull(logger, nameof(logger));
+            _serializer = Ensure.IsNotNull(serializer, nameof(serializer));
         }
 
         public Task<bool> PreBookAsync(TransferDetails transferDetails)
@@ -36,17 +53,27 @@
             throw new System.NotImplementedException();
         }
 
-        public Task<string> BookAsync(TransferDetails transferDetails)
+        public async Task<string> BookAsync(TransferDetails transferDetails)
         {
+            var request = new Request();
             try
             {
                 var supplierReferenceValues = SplitSupplierReference(transferDetails);
-                return Task.FromResult("failed");
+
+                request = await BuildRequestAsync(transferDetails, supplierReferenceValues);
+
+                await request.Send(_httpClient, _logger);
+
+                return "failed";
             }
             catch (ArgumentException ex)
             {
                 transferDetails.Warnings.AddNew("ArgumentException", ex.Message);
-                return Task.FromResult("failed");
+                return "failed";
+            }
+            finally
+            {
+                transferDetails.AddLog("Book", request);
             }
         }
 
@@ -103,5 +130,74 @@
             }
             return result;
         }
+
+        private async Task<Request> BuildRequestAsync(TransferDetails transferDetails,
+            List<SupplierReferenceData> supplierReferenceData)
+        {
+            Tuple<SupplierReferenceData, string, string> dynamicLegValues = new Tuple<SupplierReferenceData, string, string>(
+                transferDetails.OneWay ? supplierReferenceData.First() : supplierReferenceData.Last(),
+                (transferDetails.OneWay ? transferDetails.DepartureDate : transferDetails.ReturnDate).ToString("yyyy-MM-dd"),
+                transferDetails.OneWay ? transferDetails.DepartureTime : transferDetails.ReturnTime
+                );
+
+            var bookingData = BuildBookingDataAsync(transferDetails, dynamicLegValues);
+
+            var request = new Request()
+            {
+                EndPoint = _settings.URL(transferDetails),
+                Method = RequestMethod.POST,
+                ContentType = ContentTypes.Text_xml
+            };
+            var xmlDocument = Serialize(await bookingData);
+            request.SetRequest(xmlDocument);
+            return request;
+        }
+
+        private Task<AddServiceRequest> BuildBookingDataAsync(TransferDetails transferDetails,
+            Tuple<SupplierReferenceData, string, string> dynamicLegValues)
+        {
+            var addServiceRequest = new AddServiceRequest()
+            {
+                AgentID = _settings.AgentId(transferDetails),
+                Password = _settings.Password(transferDetails),
+                Opt = dynamicLegValues.Item1.Opt,
+                RateId = dynamicLegValues.Item1.RateId,
+                ExistingBookingInfo = "",
+                DateFrom = dynamicLegValues.Item2,
+                NewBookingInfo = new NewBookingInformation
+                {
+                    Name = $"{transferDetails.LeadGuestFirstName} {transferDetails.LeadGuestLastName}"
+                },
+                RoomConfigs = new RoomConfigurations()
+                {
+                    RoomConfig = new RoomConfiguration()
+                    {
+                        Adults = transferDetails.Adults,
+                        Children = transferDetails.Children,
+                        Infants = transferDetails.Infants,
+                        PaxList = transferDetails.Passengers.Select(x => new PaxDetails
+                        {
+                            Title = x.Title,
+                            Forename = x.FirstName,
+                            Surname = x.LastName,
+                            PaxType = x.PassengerType.ToString().ToCharArray()[0].ToString()
+                        }).ToList()
+                    }
+                },
+                PickUp_Date = dynamicLegValues.Item2,
+                PuTime = dynamicLegValues.Item3,
+                PuRemark = ""
+            };
+
+            return Task.FromResult(addServiceRequest);
+        }
+
+        public XmlDocument Serialize(AddServiceRequest request)
+        {
+            var xmlRequest = _serializer.SerializeWithoutNamespaces(request);
+            xmlRequest.InnerXml = $"<Request>{xmlRequest.InnerXml}</Request>";
+            return xmlRequest;
+        }
+
     }
 }
