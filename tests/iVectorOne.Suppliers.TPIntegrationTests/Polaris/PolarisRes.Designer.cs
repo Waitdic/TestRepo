d0af6c8509b177ba5c72a366257188cf4aaae5a4
WAVEACCESS\Marat.Nasyrov <Marat.Nasyrov@waveaccess.ru>
2022-12-09 17:41:24 +0300

    857 polaris update test res files

--- tests/iVectorOne.Suppliers.TPIntegrationTests/Polaris/PolarisRes.Designer.cs
+++ tests/iVectorOne.Suppliers.TPIntegrationTests/Polaris/PolarisRes.Designer.cs
@@ -61,16 +61,23 @@ namespace iVectorOne.Tests.Polaris {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to URL: https://xmldev-xml.centriumres.com/
+        ///   Looks up a localized string similar to URL: https://tst-polarisapi.avoristravel.com/avail/availability
         ///
         ///**HEADERS**
         ///
+        ///Authorization: Basic bmV0OlRlc3ROZXQwMQ==
         ///Timeout: 100
         ///
         ///
         ///**REQUEST**
         ///
-        ///&lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;&lt;SearchRequest xmlns:xsi=&quot;http://www.w3.org/2001/XMLSchema-instance&quot; xmlns:xsd=&quot;http://www.w3.org/2001/XMLSchema&quot;&gt;&lt;LoginDetails&gt;&lt;Login&gt;ClassicCollection&lt;/Login&gt;&lt;Password&gt;**********&lt;/Password&gt;&lt;Version&gt;6.0&lt;/Version&gt;&lt;/LoginDetails&gt;&lt;SearchDetails&gt;&lt;ArrivalDate&gt;2021-09-01&lt;/ArrivalDate&gt;&lt;Duration&gt;5&lt;/Duration&gt;&lt;PropertyID&gt;102245&lt;/PropertyID&gt;&lt;MealBasisID&gt;0&lt;/MealBasisID&gt;&lt;MinStarRating&gt;0&lt;/Min [rest of string was truncated]&quot;;.
+        ///{&quot;hotelAvailability&quot;:{&quot;searchAvail&quot;:{&quot;checkIn&quot;:&quot;2021-09-01&quot;,&quot;checkOut&quot;:&quot;2021-09-06&quot;,&quot;destination&quot;:{&quot;hotelCodes&quot;:[&quot;102245&quot;],&quot;location&quot;:{&quot;destinationCode&quot;:&quot;MAD_12&quot;,&quot;type&quot;:&quot;EMPTY&quot;}},&quot;rooms&quot;:[{&quot;index&quot;:1,&quot;passengerAges&quot;:[30]}]}}}
+        ///URL: https://tst-polarisapi.avoristravel.com/avail/availability
+        ///
+        ///**HEADERS**
+        ///
+        ///Authorization: Basic bmV0OlRlc3ROZXQwMQ==
+        ///Tim [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string RequestLog {
             get {
@@ -79,20 +86,25 @@ namespace iVectorOne.Tests.Polaris {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to &lt;SearchResponse xmlns:xsd=&quot;http://www.w3.org/2001/XMLSchema&quot;
-        ///    xmlns:xsi=&quot;http://www.w3.org/2001/XMLSchema-instance&quot;&gt;
-        ///    &lt;RequestInfo&gt;
-        ///        &lt;Timestamp&gt;1650658518&lt;/Timestamp&gt;
-        ///        &lt;TimestampISO&gt;2022-04-22T20:15:18+00:00&lt;/TimestampISO&gt;
-        ///        &lt;Host&gt;xmldev-xml.centriumres.com&lt;/Host&gt;
-        ///        &lt;HostIP&gt;172.31.11.218&lt;/HostIP&gt;
-        ///        &lt;ReqID&gt;62630cd63e3182.08315929&lt;/ReqID&gt;
-        ///    &lt;/RequestInfo&gt;
-        ///    &lt;ReturnStatus&gt;
-        ///        &lt;Success&gt;True&lt;/Success&gt;
-        ///        &lt;Exception/&gt;
-        ///    &lt;/ReturnStatus&gt;
-        ///    &lt;Proper [rest of string was truncated]&quot;;.
+        ///   Looks up a localized string similar to {
+        ///  &quot;request&quot;: {
+        ///    &quot;hotelAvailability&quot;: {
+        ///      &quot;searchAvail&quot;: {
+        ///        &quot;checkIn&quot;: &quot;2022-12-28&quot;,
+        ///        &quot;checkOut&quot;: &quot;2022-12-29&quot;,
+        ///        &quot;rooms&quot;: [{ &quot;passengerAges&quot;: [30, 7, 0], &quot;index&quot;: 1 }],
+        ///        &quot;destination&quot;: {
+        ///          &quot;hotelCodes&quot;: [&quot;803&quot;],
+        ///          &quot;location&quot;: { &quot;destinationCode&quot;: &quot;AVFBK&quot;, &quot;type&quot;: &quot;EMPTY&quot; }
+        ///        },
+        ///        &quot;market&quot;: &quot;GB&quot;
+        ///      },
+        ///      &quot;timeout&quot;: 16000,
+        ///      &quot;filterAvail&quot;: {}
+        ///    },
+        ///    &quot;token&quot;: &quot;FULL-DEFAULT-922851-5613720831349870&quot;
+        ///  },
+        ///  &quot;hotels&quot;: [ [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string Response {
             get {
@@ -101,7 +113,7 @@ namespace iVectorOne.Tests.Polaris {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to &lt;Results&gt;&lt;Result MID=&quot;17&quot; TPK=&quot;17&quot; CC=&quot;USD&quot; PRBID=&quot;1&quot; RTC=&quot;436430006&quot; RT=&quot;Palm View Garden View&quot; MBC=&quot;1&quot; AMT=&quot;5986&quot; TPR=&quot;436430006&quot; DSC=&quot;0&quot; COM=&quot;0&quot; DP=&quot;false&quot; NRF=&quot;true&quot; FIXPR=&quot;false&quot; SELLPR=&quot;0&quot; NETPR=&quot;0&quot; FREEC=&quot;false&quot; RQ=&quot;false&quot; PLA=&quot;false&quot; PLR=&quot;false&quot; MSP=&quot;0&quot;&gt;&lt;Cs&gt;&lt;/Cs&gt;&lt;/Result&gt;&lt;Result MID=&quot;17&quot; TPK=&quot;17&quot; CC=&quot;USD&quot; PRBID=&quot;1&quot; RTC=&quot;436440006&quot; RT=&quot;Beach Front Beachfront&quot; MBC=&quot;1&quot; AMT=&quot;6742&quot; TPR=&quot;436440006&quot; DSC=&quot;0&quot; COM=&quot;0&quot; DP=&quot;false&quot; NRF=&quot;true&quot; FIXPR=&quot;false&quot; SELLPR=&quot;0&quot; NETPR=&quot;0&quot; FREEC=&quot;false&quot; RQ=&quot;false&quot; PLA=&quot;false [rest of string was truncated]&quot;;.
+        ///   Looks up a localized string similar to &lt;Results&gt;&lt;Result MID=&quot;0&quot; TPK=&quot;803&quot; CC=&quot;EUR&quot; RTC=&quot;D&quot; RT=&quot;Doble&quot; MBC=&quot;SA&quot; AMT=&quot;318.31&quot; TPR=&quot;wFn1g/zm0AJpNodzSp3DKL/fb3DWj1x8O0At9qhpeVYvdDpMSR7qeoKyIHdo0V08wlOn1whDBK1f209jjLsk0Q+9G6RGULkcw8222K/ip3S2OBdfwkFQdLO7EBoeRbabiDSBWfHQS6zzfSLUIZ+iSNV+fs9XDo9WBNeBIVuV+wIIYSFJ/SVHYfPZ35cZSg6lH/mc9D84yLIxPz9WRw3ahoXNx74qiB8Yjr+MnEGZfem8Dw8Qw8x7yu0j3v08GaUt&quot; DSC=&quot;0&quot; COM=&quot;0&quot; DP=&quot;false&quot; NRF=&quot;false&quot; FIXPR=&quot;false&quot; SELLPR=&quot;0&quot; NETPR=&quot;0&quot; FREEC=&quot;false&quot; TPRC=&quot;PUBLICA&quot; RQ=&quot;false&quot; PLA=&quot;false&quot; PLR=&quot;false&quot; MSP=&quot;0&quot;&gt;&lt;Cs&gt;&lt;C SD=&quot;2022-12 [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string TransformedResponse {
             get {
