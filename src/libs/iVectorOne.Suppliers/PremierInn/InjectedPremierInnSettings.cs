4360dd6ee58ea7cb13667df2a4ba3ec42ac31eec
Danis Yakupov <waitdic161616@gmail.com>
2022-12-13 16:05:42 +0300

    870 - add settings parameters

--- src/libs/iVectorOne.Suppliers/PremierInn/InjectedPremierInnSettings.cs
+++ src/libs/iVectorOne.Suppliers/PremierInn/InjectedPremierInnSettings.cs
@@ -1,49 +1,51 @@
 ﻿namespace iVectorOne.Suppliers.PremierInn
 {
+    using Intuitive.Helpers.Extensions;
+    using iVectorOne.Constants;
     using iVectorOne.Support;
 
     public class InjectedPremierInnSettings : SettingsBase, IPremierInnSettings
     {
-        protected override string Source { get; }
+        protected override string Source => ThirdParties.PREMIERINN;
 
         public string User(IThirdPartyAttributeSearch tpAttributeSearch)
         {
-            throw new System.NotImplementedException();
+            return Get_Value("User", tpAttributeSearch);
         }
 
         public string Password(IThirdPartyAttributeSearch tpAttributeSearch)
         {
-            throw new System.NotImplementedException();
+            return Get_Value("Password", tpAttributeSearch);
         }
 
         public string GenericURL(IThirdPartyAttributeSearch tpAttributeSearch)
         {
-            throw new System.NotImplementedException();
+            return Get_Value("GenericURL", tpAttributeSearch);
         }
 
         public bool UseGZip(IThirdPartyAttributeSearch tpAttributeSearch)
         {
-            throw new System.NotImplementedException();
+            return Get_Value("UseGZip", tpAttributeSearch).ToSafeBoolean();
         }
 
         public bool AllowCancellations(IThirdPartyAttributeSearch tpAttributeSearch)
         {
-            throw new System.NotImplementedException();
+            return Get_Value("AllowCancellations", tpAttributeSearch).ToSafeBoolean();
         }
 
         public int HotelSearchLimit(IThirdPartyAttributeSearch tpAttributeSearch)
         {
-            throw new System.NotImplementedException();
+            return Get_Value("HotelSearchLimit", tpAttributeSearch).ToSafeInt();
         }
 
         public string LanguageCode(IThirdPartyAttributeSearch tpAttributeSearch)
         {
-            throw new System.NotImplementedException();
+            return Get_Value("LanguageCode", tpAttributeSearch);
         }
 
         public bool AllowOnRequest(IThirdPartyAttributeSearch tpAttributeSearch)
         {
-            throw new System.NotImplementedException();
+            return Get_Value("AllowOnRequest", tpAttributeSearch).ToSafeBoolean();
         }
     }
 }
