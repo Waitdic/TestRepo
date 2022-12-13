49d211d26f06182e4d6b6bb80f405a98b24bee7c
Danis Yakupov <waitdic161616@gmail.com>
2022-12-13 15:51:09 +0300

    870 - add book models

---
+++ src/libs/iVectorOne.Suppliers/PremierInn/Models/Book/BookingConfirmRequest.cs
@@ -0,0 +1,35 @@
+﻿namespace iVectorOne.Suppliers.PremierInn.Models.Book
+{
+    using iVectorOne.Suppliers.PremierInn.Models.Common;
+
+    public class BookingConfirmRequest
+    {
+        public Login Login { get; set; } = new();
+
+        public BookRequestParameters Parameters { get; set; } = new();
+    }
+
+    public class BookRequestParameters : Parameters
+    {
+        public Session Session { get; set; } = new();
+
+        public Rooms Rooms { get; set; } = new();
+
+        public Address Address { get; set; } = new();
+
+        public string BookingCompanyName { get; set; } = string.Empty;
+
+        public BookerDetails BookerDetails { get; set; } = new();
+
+        public string BookingType { get; set; } = string.Empty;
+
+        public string ArrivalTime { get; set; } = string.Empty;
+
+        public SpecialRequirements SpecialRequirements { get; set; } = new();
+    }
+
+    public class SpecialRequirements
+    {
+        public string Text { get; set; } = string.Empty;
+    }
+}
