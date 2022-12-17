2813b2b20d142c58b4f86de0a632f9c8430a839a
Sher Dhakar <sher.dhakar@nagarro.com>
2022-12-17 11:28:00 +0530

    IDT-1354- Unit test cases for transfer search

--- src/libs/iVectorOne.Suppliers/TourPlanTransfers/TourPlanTransfersSearchBase.cs
+++ src/libs/iVectorOne.Suppliers/TourPlanTransfers/TourPlanTransfersSearchBase.cs
@@ -62,18 +62,20 @@
 
         public Task<List<Request>> BuildSearchRequestsAsync(TransferSearchDetails searchDetails, LocationMapping location)
         {
-            LocationData tpLocations = GetThirdPartyLocations(location);
-            var Outbound = BuildOptionInfoRequest(searchDetails, tpLocations, searchDetails.DepartureDate);
             List<Request> requests = new List<Request>();
-            Outbound.ExtraInfo = Constants.Outbound;
-            requests.Add(Outbound);
-            if (!searchDetails.OneWay)
+            LocationData tpLocations = GetThirdPartyLocations(location);
+            if (!string.IsNullOrEmpty(tpLocations.LocationCode))
             {
-                var returnBuildOptionInfoRequest = BuildOptionInfoRequest(searchDetails, tpLocations, searchDetails.ReturnDate);
-                returnBuildOptionInfoRequest.ExtraInfo = string.Empty;
-                requests.Add(returnBuildOptionInfoRequest);
+                var Outbound = BuildOptionInfoRequest(searchDetails, tpLocations, searchDetails.DepartureDate);
+                Outbound.ExtraInfo = Constants.Outbound;
+                requests.Add(Outbound);
+                if (!searchDetails.OneWay)
+                {
+                    var returnBuildOptionInfoRequest = BuildOptionInfoRequest(searchDetails, tpLocations, searchDetails.ReturnDate);
+                    returnBuildOptionInfoRequest.ExtraInfo = string.Empty;
+                    requests.Add(returnBuildOptionInfoRequest);
+                }
             }
-
             return Task.FromResult(requests);
 
         }
@@ -124,7 +126,7 @@
             {
                 if (!ResponseHasExceptions(request))
                 {
-                    deserializedResponse = Helpers.DeSerialize<OptionInfoReply>(request.ResponseXML,_serializer);
+                    deserializedResponse = Helpers.DeSerialize<OptionInfoReply>(request.ResponseXML, _serializer);
 
                     if ((string)request.ExtraInfo == Constants.Outbound)
                     {
@@ -180,7 +182,7 @@
             {
                 TPSessionID = "",
                 SupplierReference = supplierReference,
-                TransferVehicle = comment,
+                TransferVehicle = comment.Substring(0, comment.Length <= 50 ? comment.Length : 50),
                 ReturnTime = "12:00",
                 VehicleCost = 0,
                 AdultCost = 0,
