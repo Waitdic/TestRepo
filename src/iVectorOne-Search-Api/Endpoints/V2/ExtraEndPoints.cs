612d9b624128f9a0a7fa83223461b562d21a7cbf
Ashish Sharma <ashish.sharma18@nagarro.com>
2022-12-26 17:31:51 +0530

    IDT-1379- Add Request and Map Endpoints-Extras

---
+++ src/iVectorOne-Search-Api/Endpoints/V2/ExtraEndPoints.cs
@@ -0,0 +1,39 @@
+﻿using Intuitive.Helpers.Extensions;
+using iVectorOne.SDK.V2.ExtraSearch;
+using iVectorOne.Web.Infrastructure.V2;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iVectorOne.Search.Api.Endpoints.V2
+{
+    public static class ExtraEndPoints
+    {
+        public static IEndpointRouteBuilder MapExtraEndpoints(this IEndpointRouteBuilder endpoints)
+        {
+            RegisterSearchEndpointsForDomain(endpoints, EndpointBase.ExtrasDomain);
+
+            _ = endpoints.MapGet("/healthcheck", () => "Hello World!").AllowAnonymous();
+
+            return endpoints;
+        }
+
+        private static void RegisterSearchEndpointsForDomain(IEndpointRouteBuilder endpoints, string domain)
+        {
+            _ = endpoints
+                .MapPost(
+                    $"/{EndpointBase.Version}/{domain}/search",
+                    async (HttpContext httpContext, [FromServices] IMediator mediator, [FromBody] Request request)
+                        => await EndpointBase.ExecuteRequest<Request, Response>(httpContext, mediator, request))
+                .RequireAuthorization()
+                .WithName($"{domain.ToProperCase()} Search")
+                .ProducesValidationProblem(StatusCodes.Status400BadRequest)
+                .Produces(StatusCodes.Status200OK);
+        }
+
+        private static List<int> SetChildAges(string childAges)
+        {
+            return childAges.Split(',').Where(m => int.TryParse(m, out _)).Select(m => int.Parse(m)).ToList();
+        }
+
+    }
+}
