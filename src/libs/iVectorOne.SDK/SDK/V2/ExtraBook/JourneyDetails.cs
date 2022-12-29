c60e15e8462b2f3c3bd4dd07cd6df0b966c40b19
Ashish Sharma <ashish.sharma18@nagarro.com>
2022-12-29 16:35:03 +0530

    IDT-1393|Prebook,book base checkin for extras

---
+++ src/libs/iVectorOne.SDK/SDK/V2/ExtraBook/JourneyDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace iVectorOne.SDK.V2.ExtraBook
+{
+    /// <summary>
+    /// a class representing journey details as part of a transfer booking
+    /// </summary>
+    public class JourneyDetails
+    {
+        /// <summary>
+        /// Gets or sets the flight code.
+        /// </summary>
+        public string FlightCode { get; set; } = string.Empty;
+    }
+}
