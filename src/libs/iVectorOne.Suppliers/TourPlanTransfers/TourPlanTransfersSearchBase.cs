62e5d6dda8664cb204929061890f61a9cc909344
Sher Dhakar <sher.dhakar@nagarro.com>
2023-01-03 17:52:39 +0530

    IDT-1401:Formatting issue

--- src/libs/iVectorOne.Suppliers/TourPlanTransfers/TourPlanTransfersSearchBase.cs
+++ src/libs/iVectorOne.Suppliers/TourPlanTransfers/TourPlanTransfersSearchBase.cs
@@ -150,7 +150,7 @@
                 {
                     supplierReference = Helpers.CreateSupplierReference(outboundResult.Opt, outboundResult.OptStayResults.RateId, "", "");
                     transformedResult = BuildTransformedResult(outboundResult.OptStayResults, supplierReference, outboundResult.OptGeneral
-                        .Comment, outboundResult.OptStayResults.TotalPrice);
+                                                               .Comment, outboundResult.OptStayResults.TotalPrice);
                     transformedResultList.Add(transformedResult);
                 }
             }
@@ -159,12 +159,12 @@
                 foreach (var outboundResult in filteredOutbound.Option)
                 {
                     foreach (var returnResult in filteredReturn.Option.Where(x => x.OptGeneral.Comment == outboundResult.OptGeneral.Comment
-                    && x.OptStayResults.Availability == outboundResult.OptStayResults.Availability))
+                                                                             && x.OptStayResults.Availability == outboundResult.OptStayResults.Availability))
                     {
                         supplierReference = Helpers.CreateSupplierReference(outboundResult.Opt, outboundResult.OptStayResults.RateId,
-                            returnResult.Opt, returnResult.OptStayResults.RateId);
-                        transformedResult = BuildTransformedResult(returnResult.OptStayResults, supplierReference, outboundResult.OptGeneral
-                            .Comment, returnResult.OptStayResults.TotalPrice + outboundResult.OptStayResults.TotalPrice);
+                                                                            returnResult.Opt, returnResult.OptStayResults.RateId);
+                        transformedResult = BuildTransformedResult(returnResult.OptStayResults, supplierReference, outboundResult.OptGeneral.Comment
+                                                                   , returnResult.OptStayResults.TotalPrice + outboundResult.OptStayResults.TotalPrice);
                         transformedResultList.Add(transformedResult);
                     }
                 }
