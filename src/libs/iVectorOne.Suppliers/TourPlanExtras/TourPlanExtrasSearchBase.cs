160d207c9d11213ebf09e43d8db54ab8aa800775
Jyoti Verma <jyoti.verma@nagarro.com>
2023-01-05 13:45:00 +0530

    Added folder sturcture for TourPlanExtras

---
+++ src/libs/iVectorOne.Suppliers/TourPlanExtras/TourPlanExtrasSearchBase.cs
@@ -0,0 +1,42 @@
+﻿namespace iVectorOne.Suppliers.TourPlanExtras
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Intuitive.Helpers.Net;
+    using iVectorOne.Extra;
+    using iVectorOne.Interfaces;
+    using iVectorOne.Models;
+    using iVectorOne.Search.Models;
+    using iVectorOne.Search.Results.Models.Extra;
+
+    public abstract class TourPlanExtrasSearchBase : IThirdPartySearch, ISingleSource
+    {
+        public abstract string Source { get; }
+
+        public Task<List<Request>> BuildSearchRequestsAsync(ExtraSearchDetails searchDetails, LocationMapping location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ResponseHasExceptions(Request request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SearchRestrictions(ExtraSearchDetails searchDetails)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TransformedExtraResultCollection TransformResponse(List<Request> requests, ExtraSearchDetails searchDetails, LocationMapping location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateSettings(ExtraSearchDetails searchDetails)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
