93d9ffc56992eacd12c354d2abea3a61d970fba4
Jyoti Verma <jyoti.verma@nagarro.com>
2023-01-09 16:19:23 +0530

    IDT:1411: Addressed comments

--- src/libs/iVectorOne.Service/Repositories/LocationRepository.cs
+++ src/libs/iVectorOne.Service/Repositories/LocationRepository.cs
@@ -13,28 +13,20 @@
         private const int _timeout = 2;
 
         private readonly ISql _sql;
-        private readonly IMemoryCache _cache;
 
-        public LocationRepository(ISql sql,IMemoryCache cache)
+        public LocationRepository(ISql sql)
         {
             _sql = sql;
-            _cache = cache;
         }
 
         /// <inheritdoc />
         public async Task<List<Location>> GetAllLocations(string source)
         {
-            string cacheKey = "TPLocationContentLookup_" + source;
-
-            async Task<List<Location>> cacheBuilder()
-            {
-                return await _sql.ReadSingleMappedAsync(
-                    "select IVOLocationID as LocationID, Description  from IVOLocation where Source = @source",
-                    async r => (await r.ReadAllAsync<Location>()).ToList(),
-                new CommandSettings().WithParameters(new { source }));
-            }
-
-            return await _cache.GetOrCreateAsync(cacheKey, cacheBuilder, _timeout);
+            var locations = await _sql.ReadSingleMappedAsync(
+                 "select IVOLocationID as LocationID, Description  from IVOLocation where Source = @source",
+                 async r => (await r.ReadAllAsync<Location>()).ToList(),
+             new CommandSettings().WithParameters(new { source }));
+            return locations;
         }
     }
 }
