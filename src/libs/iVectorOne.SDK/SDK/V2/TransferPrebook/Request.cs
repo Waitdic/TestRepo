fcc28691886e0334195e79b8464e211ef1105d7b
davidy <davidy@intuitivesystems.co.uk>
2023-01-16 10:33:55 +0800

    IDT-1060 remove commented code

--- src/libs/iVectorOne.SDK/SDK/V2/TransferPrebook/Request.cs
+++ src/libs/iVectorOne.SDK/SDK/V2/TransferPrebook/Request.cs
@@ -1,6 +1,5 @@
 ﻿namespace iVectorOne.SDK.V2.TransferPrebook
 {
-    using System.Collections.Generic;
     using MediatR;
 
     public record Request : TransferRequestBase, IRequest<Response>
@@ -12,15 +11,5 @@
 
         /// <summary>Gets or sets the supplier reference.</summary>
         public string SupplierReference { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Get or sets the unique nationality identifier 
-        /// </summary>
-        //public string NationalityID { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Gets or sets the selling country.
-        /// </summary>
-        //public string SellingCountry { get; set; } = string.Empty;
     }
 }
